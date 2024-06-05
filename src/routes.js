9ebd9618e87d9ce91f08437cc3b5c1dd0bfbab66
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-06-05 15:06:07 -0300

    finish crud

--- src/routes.js
+++ src/routes.js
@@ -26,8 +26,55 @@ export const routes = [
     method: 'GET',
     path: buildRoutePath('/tasks'),
     handler: (req, res) => {
-      const tasks = db.select('tasks')
+      const { search } = req.query
+      const tasks = db.select('tasks', search ? {
+        title: search,
+        description: search
+      }: null)
+      
       return res.end(JSON.stringify(tasks))
+    }
+  },
+  {
+    method: 'PUT',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const { title, description } = req.body
+      if (!title && !description) {
+        return res.end(JSON.stringify({message: `required fields for update`}))
+      }
+      const [task] = db.select('tasks', { id })
+      if(!task) return res.writeHead(404).end(JSON.stringify({message: 'not found'}))
+      db.update('tasks', id, {
+        updated_at: new Date(),
+        title: title ?? task.title,
+        description: description ?? task.description
+
+      })
+      res.end(JSON.stringify({message: 'ok'}))
+    }
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/tasks/:id'),
+    handler: (req, res) => {
+      const { id } = req.params
+      db.delete('tasks', id)
+      return res.writeHead(204).end()
+    }
+  },
+  {
+    method: 'PATCH',
+    path: buildRoutePath('/tasks/:id/complete'),
+    handler: (req, res) => {
+      const { id } = req.params
+      const [task] = db.select('tasks', { id })
+      if (!task) return res.writeHead(404).end(JSON.stringify({ message: 'not found' }))
+      db.update('tasks', id, {
+        updated_at: new Date(),
+        completed_at: task.completed_at ? null : new Date()
+      })
     }
   }
 ]
